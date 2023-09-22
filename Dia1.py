711ce32cf36a6e3ea26d4084f9d09d25186f79df
CeliaSTEM <145722929+CeliaSTEM@users.noreply.github.com>
2023-09-22 11:28:45 +0200

    Print+Str+Int
    Print.
    String: Concatenación de String con valores numéricos + Transformación de números a string.
    Int: Operaciones aritméticas.

---
+++ Dia1.py
@@ -0,0 +1,57 @@
+#Que bonito es Python
+print("Hola Mundo")
+print("Me llamo Celia")
+#Nuestra primera variable
+x = 5
+print("print(5):")
+print(5)                #Imprime 5.
+print("print(5+5):") 
+print(5+5)              #Imprime 10.
+print("print(x):")
+print(x)                #5
+print("print(x+10):")
+print(x+10)
+#Creamos una variable con texto -> string
+nombre = "Celia"
+numero = 8
+print(nombre)
+print("Sumo nombre+numero:")
+print('print("Celia"+"8"):')
+print("Celia"+"8")
+print('print(nombre+str(numero)):')
+print(nombre+str(numero))
+print("Escribo 10 veces CuCu")
+print("CuCu"*10)
+print()
+print("\n\\Hacemos \"operaciones\"\t matemáticas\\")
+num1 = 2
+num2 = 8
+suma = num1+num2
+resta = num1-num2
+multp = num1*num2
+div = num1/num2
+print("Suma = " + str(suma) + "\
+| Resta = " + str(resta) + "\
+| Multiplicacion = " + str(multp) + "\
+| Division = " + str(div))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
